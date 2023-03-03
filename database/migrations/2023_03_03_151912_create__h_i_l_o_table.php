8c6378b5be78a893082d590ef1f41911c4c0bd05
Andres <jao9@alu.ua.es>
2023-03-03 17:20:26 +0100

    Final base de datos

--- database/migrations/2023_03_03_151912_create__h_i_l_o_table.php
+++ database/migrations/2023_03_03_151912_create__h_i_l_o_table.php
@@ -13,26 +13,27 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('hilos', function (Blueprint $table) {
+
+         Schema::create('hilos', function (Blueprint $table) {
             $table->id();
             $table->text('texto');
             $table->string('imagen')->nullable();
             $table->timestamp('fecha');
+            $table->unsignedBigInteger('categoria_id')->nullable();
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('set null');
         });
 
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('id_hilo');
-            $table->foreign('id_hilo')->references('id')->on('hilos')->onDelete('cascade');
-            $table->string('hastag');
+            $table->string('hashtag');
             $table->integer('views');
             $table->string('imagen')->nullable();
         });
 
         Schema::create('tuits', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('id_hilo');
-            $table->foreign('id_hilo')->references('id')->on('hilos')->onDelete('cascade');
+            $table->unsignedBigInteger('hilo_id');
+            $table->foreign('hilo_id')->references('id')->on('hilos')->onDelete('cascade');
             $table->text('texto');
             $table->string('imagen')->nullable();
             $table->timestamp('fecha');
