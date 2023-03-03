f2197622b3a96cbfdc54259b3a081b4b8539c609
Andres <jao9@alu.ua.es>
2023-03-03 18:58:05 +0100

    Arreglos de los seeders

---
+++ app/Models/Categoria.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    use HasFactory;
+
+    public $timestamps =false;
+
+    protected $table = 'categorias';
+
+    protected $fillable = ['hashtag', 'views', 'imagen'];
+
+    public function hilo()
+    {
+        return $this->belongsTo(Hilo::class);
+    }
+}
---
+++ app/Models/Hilo.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hilo extends Model
+{
+    use HasFactory;
+
+    public $timestamps =false;
+
+    protected $table = 'hilos';
+
+    protected $fillable = ['texto', 'imagen', 'fecha'];
+
+    public function categoria()
+    {
+        return $this->hasMany(Categoria::class);
+    }
+
+    public function tuit()
+    {
+        return $this->hasMany(Tuit::class);
+    }
+}
