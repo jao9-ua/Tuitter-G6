97dc55b6434c70cdf8edc5b1d374115d7adc2852
Daniel Regueira <daniel.regueira.ayuso@gmail.com>
2023-04-03 15:51:50 +0200

    Cambios UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,9 +7,18 @@ use App\Models\Usuario;
 
 class UserController extends Controller
 {
+    public function index()
+    {
+        $usuarios = Usuario::all();
+        
+        return view('usuarios.index', ['usuarios' => $usuarios]);
+    }
+
     public function show($id)
     {
         $user = Usuario::find($id);
         return view('users.show', ['user' => $user]);
     }
+
+    //public function edit($id) {}
 }
