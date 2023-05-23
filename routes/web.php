79cd66ad9d3ad02b7657c2ec2233b595d54876bd
Daniel Regueira <daniel.regueira.ayuso@gmail.com>
2023-05-23 20:41:03 +0200

    blade.php para listar hilos y tuits de un usuario

--- routes/web.php
+++ routes/web.php
@@ -62,7 +62,7 @@ Route::delete('/tuits/{id}', 'TuitController@destroy')->name('tuits.destroy');
 Route::get('/hilos/{idHilo}/tuits/create', [TuitController::class, 'create'])->name('tuits.create');
 Route::post('/tuits', [TuitController::class, 'store'])->name('tuits.store');
 Route::get('/tuits/crear/{hilo}', [TuitController::class, 'crear'])->name('tuits.crear');
-Route::get('/tuits/usuario/{usuarioID}', [TuitController::class, 'index'])->name('tuits.tuitsUsuario');
+Route::get('/tuits/usuario/{usuarioID}', [TuitController::class, 'index'])->name('tuits.tuitsUsuario'); //modificar 'index' a 'tuitsUsuario'
 
 //Rutas hilos
 Route::get('/hilos', [HiloController::class, 'index'])->name('hilos.index');
@@ -72,7 +72,7 @@ Route::post('/hilos/{id}/tuits', [HiloController::class, 'store'])->name('tuit.s
 Route::put('/hilos/{id}', 'HiloController@update')->name('hilo.update');
 Route::delete('/hilos/{id}', 'HiloController@destroy')->name('hilos.destroy');
 Route::get('/hilo/crear', [HiloController::class, 'crear'])->name('hilo.crear');
-Route::get('/hilos/usuario/{usuarioID}', [HiloController::class, 'index'])->name('hilos.hilosUsuario');
+Route::get('/hilos/usuario/{usuarioID}', [HiloController::class, 'index'])->name('hilos.hilosUsuario'); //modificar 'index' a 'hilosUsuario'
 
 //Rutas categorias
 Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
