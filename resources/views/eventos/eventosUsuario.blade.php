351d4bc56f0b85216012395b2256c863830094bf
Daniel Regueira <daniel.regueira.ayuso@gmail.com>
2023-05-24 13:56:25 +0200

    Solo admin/propietario puede Eliminar/Editar eventos

--- resources/views/eventos/eventosUsuario.blade.php
+++ resources/views/eventos/eventosUsuario.blade.php
@@ -43,8 +43,10 @@
                     <td>{{ $evento->fecha_fin }}</td>
                     <td>
                         <a href="{{ route('eventos.show', $evento->id) }}" class="btn btn-primary btn-sm">Ver</a>
+                        @if (auth()->user()->id === $evento->usuario_id)
                         <a href="{{ route('eventos.edit', $evento->id) }}" class="btn btn-secondary btn-sm">Editar</a>
-                        @if (auth()->user()->es_Admin)
+                        @endif
+                        @if (auth()->user()->es_Admin || auth()->user()->id === $evento->usuario_id)
                         <form action="{{ route('evento.destroy', $evento->id) }}" method="POST" class="d-inline">
                             @csrf
                             @method('DELETE')
